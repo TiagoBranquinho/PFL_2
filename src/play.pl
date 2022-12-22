e9ae0c63222063bd173aab2e8b7911962376751d
Tiago <t.branquinho.slb@gmail.com>
2022-12-22 14:19:24 +0000

    Finished move function

--- src/play.pl
+++ src/play.pl
@@ -7,22 +7,29 @@ update_game(Gamestate) :-
     display_game(Gamestate),
     retrieve_move(Gamestate, Move),
     %print_valid_moves(Move),
-    nth0(0, Move, FirstMove),
-    nth0(1, Move, SecondMove),
-    move(Gamestate, FirstMove, NewGamestate),
-    move(NewGamestate, SecondMove, LastGamestate),
-    update_game(LastGamestate).
+    move(Gamestate, Move, NewGamestate),
+    write('final final final print\n'),
+    getCurrBoard(NewGamestate, Board),
+    display_board(Board).
+    %update_game(Gamestate).
 
 
 retrieve_move(Gamestate, Move):- 
     retrieve_move_menu(Gamestate, Move).
 
 
-move(Gamestate, CurrMove, NewGamestate):- 
+move(Gamestate, [], Gamestate).
+
+
+move(Gamestate, [CurrMove|NextMove], NewGamestate):- 
     getCurrBoard(Gamestate, Board),
     getCurrPlayer(Gamestate, Player),
     %printmove(CurrMove),
     get_move_info(CurrMove, X, Y, P),
     update_matrix(Board, X, Y, P, NewBoard),    
     %display_board(NewBoard),
-    create_game_state(NewPlayer, NewBoard, NewGamestate).
+    create_game_state(Player, NewBoard, TempGamestate),
+    %getCurrBoard(NewGameState, Boarde),
+    %write('final board\n'),
+    %display_board(Boarde),
+    move(TempGamestate, NextMove, NewGamestate).    
